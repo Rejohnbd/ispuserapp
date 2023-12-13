f69856fb771c41ca5b91186dbc1380bde6fa8c39
unknown <rejohn.cse@gmail.com>
2023-12-13 22:00:54 +0600

    Add Data table to invoices

--- resources/views/web/showinvoices.blade.php
+++ resources/views/web/showinvoices.blade.php
@@ -24,7 +24,7 @@
     <div class="card">
         <h5 class="card-header">Invoices</h5>
         <div class="p-2 card-datatable table-responsive">
-            <table id="@if(count($showinvoices) > 0) example @endif" class="dt-responsive table table-bordered">
+            <table id="example" class="dt-column-search table table-bordered">
                 <thead>
                     <tr>
                         <th>#ID</th>
@@ -69,9 +69,13 @@
 @endsection
 
 @section('styles')
-{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" /> --}}
-<link href="https://cdn.datatables.net/v/dt/dt-1.13.8/datatables.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
+<!-- DataTables Responsive Bootstrap 5 CSS -->
+<link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
+
+<!-- Flatpickr CSS -->
+<link rel="stylesheet" href="{{ asset('assets/vendor/libs/flatpickr/flatpickr.css') }}" />
+
 <style>
     table td {
         max-width: 500px;
@@ -90,9 +94,9 @@
         height: 20px !important;
         font-size: 12px !important;
         font-weight: bold !important;
-        color: white !important;
-        background: rgb(2, 0, 36);
-        background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);
+        color: rgb(12, 12, 12) !important;
+        /* background: rgb(2, 0, 36);
+            background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%); */
     }
 
     .dt-button.buttons-csv {
@@ -134,19 +138,94 @@
     }
 
     .table:not(.table-dark) thead:not(.table-dark) th {
-        color: #fff !important;
+        color: #131212 !important;
+    }
+
+    .control-sm {
+        height: calc(1.25rem + 10px) !important;
     }
 </style>
 @endsection
 
 @section('scripts')
-{{-- <script src="{{ asset('assets/vendor/libs/datatables/jquery.dataTables.js') }}"></script>
+<!-- Vendors JS -->
+<script src="{{ asset('assets/vendor/libs/datatables/jquery.dataTables.js') }}"></script>
 <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
 <script src="{{ asset('assets/vendor/libs/datatables-responsive/datatables.responsive.js') }}"></script>
 <script src="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.js') }}"></script>
-<script src="{{ asset('assets/js/tables-datatables-advanced.js') }}"></script> --}}
-<script src="https://cdn.datatables.net/v/dt/dt-1.13.8/datatables.min.js"></script>
+
+<!-- Flat Picker -->
+<script src="{{ asset('assets/vendor/libs/moment/moment.js') }}"></script>
+<script src="{{ asset('assets/vendor/libs/flatpickr/flatpickr.js') }}"></script>
+
+<!-- Main JS -->
+{{-- <script src="{{ asset('assets/js/main.js') }}"></script> --}}
+
 <script>
-    new DataTable('#example');
+    $(document).ready(function() {
+        // Setup - add a text input to each footer cell
+        $('#example thead tr')
+            .clone(true)
+            .addClass('filters')
+            .appendTo('#example thead');
+
+        var table = $('#example').DataTable({
+            orderCellsTop: true,
+            fixedHeader: true,
+            initComplete: function() {
+                var api = this.api();
+
+                // For each column
+                api
+                    .columns()
+                    .eq(0)
+                    .each(function(colIdx) {
+                        // Set the header cell to contain the input element
+                        var cell = $('.filters th').eq(
+                            $(api.column(colIdx).header()).index()
+                        );
+                        var title = $(cell).text();
+                        $(cell).html(
+                            '<input type="text" class="form-control control-sm" placeholder="' +
+                            title + '" />');
+
+                        // On every keypress in this input
+                        $(
+                                'input',
+                                $('.filters th').eq($(api.column(colIdx).header()).index())
+                            )
+                            .off('keyup change')
+                            .on('change', function(e) {
+                                // Get the search value
+                                $(this).attr('title', $(this).val());
+                                var regexr =
+                                    '({search})'; //$(this).parents('th').find('select').val();
+
+                                var cursorPosition = this.selectionStart;
+                                // Search the column for that value
+                                api
+                                    .column(colIdx)
+                                    .search(
+                                        this.value != '' ?
+                                        regexr.replace('{search}', '(((' + this.value +
+                                            ')))') :
+                                        '',
+                                        this.value != '',
+                                        this.value == ''
+                                    )
+                                    .draw();
+                            })
+                            .on('keyup', function(e) {
+                                e.stopPropagation();
+
+                                $(this).trigger('change');
+                                $(this)
+                                    .focus()[0]
+                                    .setSelectionRange(cursorPosition, cursorPosition);
+                            });
+                    });
+            },
+        });
+    });
 </script>
 @endsection
